723025c6a62af575dad3b2146157271e96864d93
Altaf Quadri <altafmquadri@gmail.com>
2020-06-07 18:04:10 -0400

    installed react-dates and moment, and setup singledatepicker

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import moment from 'moment'
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates'
+import 'react-dates/lib/css/_datepicker.css'
+
+const now = moment()
+console.log(now.format('MMM Do, YYYY'));
 
 class ExpenseForm extends Component {
     state = {
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        createdAt: moment(),
+        calendarFocused: false
     }
 
     onDescriptionChange = e => {
@@ -22,6 +31,14 @@ class ExpenseForm extends Component {
         if (amount.match(/^\d*(\.\d{0,2})?$/)) {
             this.setState({ amount })
         }
+    }
+
+    onDateChange = createdAt => {
+        this.setState({ createdAt })
+    }
+
+    onFocusChange = ({ focused }) => {
+        this.setState({ calendarFocused: focused });
     }
 
 
@@ -42,6 +59,14 @@ class ExpenseForm extends Component {
                         value={this.state.amount}
                         onChange={this.onAmountChange}>
                     </input>
+                    <SingleDatePicker
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={() => false}
+                    />
                     <textarea
                         placeholder='add a note for your expense (optional)'
                         value={this.state.note}
