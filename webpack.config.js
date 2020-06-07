82cd291996abbe91c36bc28ec4504428ffe31dfb
Altaf Quadri <altafmquadri@gmail.com>
2020-06-07 17:05:21 -0400

    setting up form

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/playground/redux-expensify.js',
+    entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js',
