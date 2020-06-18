ed869820a4e1291b75e425850251dc0a59977c69
Altaf Quadri <altafmquadri@gmail.com>
2020-06-18 12:13:15 -0400

    changed scripts, but still see the same size builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,36 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
-        publicPath: '/'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: ['babel-loader', 'eslint-loader']
+module.exports = (env) => {
+    const isProduction = env === 'production'
+    console.log(env);
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js',
+            publicPath: '/'
         },
-        {
-            test: /\.s?css$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }]
-    },
-    devtool: 'cheap-module-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true,
-        port: 3000
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html'
-    })]
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader']
+            },
+            {
+                test: /\.s?css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true,
+            port: 3000
+        },
+        plugins: [new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })]
+    }
 }
+
