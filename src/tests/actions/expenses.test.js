6eec6d8d49795ffd3cc26610fd9b3b6f9cc8e58a
Altaf Quadri <altafmquadri@gmail.com>
2020-06-24 15:32:23 -0400

    failing test in expenses.test.js because of done method

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,9 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses'
+import expenses from '../fixtures/expenses'
+
+const createMockStore = configureMockStore([thunk])
 
 
 test('should setup remove expense action object', () => {
@@ -21,36 +26,44 @@ test('should edit expense action object', () => {
 })
 
 test('should setup add expense action object with provided values', () => {
-    const expenseData = {
-        description: 'Rent',
-        amount: 109500,
-        createdAt: 1000,
-        note: 'This was last months rent'
-    }
-    const action = addExpense(expenseData)
+    const action = addExpense(expenses[2])
     expect(action).toEqual({
         type: 'ADD_EXPENSE',
-        expense: {
-            ...expenseData,
-            id: expect.any(String)
-        }
+        expense: expenses[2]
     })
 })
 
-
-test('should setup add expense action object with default values', () => {
+test('should add expense to database and store', (done) => {
+    const store = createMockStore({})
     const expenseData = {
-        description: '',
-        amount: 0,
-        createdAt: 0,
-        note: ''
+        description: 'Mouse',
+        amount: 3000,
+        note: 'This one is better',
+        createdAt: 1000
     }
-    const action = addExpense()
-    expect(action).toEqual({
-        type: 'ADD_EXPENSE',
-        expense: {
-            ...expenseData,
-            id: expect.any(String)
-        }
+    store.dispatch(startAddExpense(expenseData)).then(() => {
+        expect(1).toBe(2);
+        done();
     })
 })
+
+test('should add expense with defaults to database and store', () => {
+
+})
+
+// test('should setup add expense action object with default values', () => {
+//     const expenseData = {
+//         description: '',
+//         amount: 0,
+//         createdAt: 0,
+//         note: ''
+//     }
+//     const action = addExpense()
+//     expect(action).toEqual({
+//         type: 'ADD_EXPENSE',
+//         expense: {
+//             ...expenseData,
+//             id: expect.any(String)
+//         }
+//     })
+// })
