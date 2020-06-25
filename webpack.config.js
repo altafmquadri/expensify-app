8574096a5669d9d04276100323bb111563ef123b
Altaf Quadri <altafmquadri@gmail.com>
2020-06-25 17:32:58 -0400

    trying to debug failure of database url

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = (env) => {
             'proces.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
             'proces.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
             'proces.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-            'proces.env.FIREBASE_PROJECTID': JSON.stringify(process.env.FIREBASE_PROJECTID),
+            'proces.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
             'proces.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
             'proces.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
             'proces.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
